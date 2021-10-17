51910e33bcaa8e4c7081e3d8921194d45d9f4c92
guilbd@hotmail.com <guilbd@hotmail.com>
2021-10-17 02:23:13 -0300

    class-validator

--- src/users/dtos/create.user.dto.ts
+++ src/users/dtos/create.user.dto.ts
@@ -1,4 +1,8 @@
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'Informe um endereço de email' })
+  @IsEmail({}, { message: 'Informe um endereço de e-mail válido' })
   email: string;
   name: string;
   password: string;
