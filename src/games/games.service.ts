812597df2031501cfb0efea47ee6cf0dd1127f3f
guilbd <guilbd@hotmail.com>
2021-10-19 21:07:57 -0300

    function create

--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnprocessableEntityException,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { CreateGameDto } from './dto/create-game.dto';
 
 @Injectable()
-export class GamesService {}
+export class GamesService {
+  constructor(private createGameDto: CreateGameDto) {}
+
+  async createGame(createGameDto: CreateGameDto): Promise
+}
